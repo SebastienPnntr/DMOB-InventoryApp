cd484338f84bc266d161b14ff5e35d34d241d8b9
1Sebi <sebastien.pnntr@eduge.ch>
2023-03-13 14:18:34 +0100

    Etape 07 - 02

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -2,6 +2,7 @@ package com.example.inventory.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
@@ -11,6 +12,17 @@ abstract class ItemRoomDatabase : RoomDatabase(){
         @Volatile
         private var INSTANCE: ItemRoomDatabase? = null
 
-        fun getDatabase(context: Context): ItemRoomDatabase {}
+        fun getDatabase(context: Context): ItemRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ItemRoomDatabase::class.java,
+                    "item_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                return instance
+            }
+        }
     }
 }
