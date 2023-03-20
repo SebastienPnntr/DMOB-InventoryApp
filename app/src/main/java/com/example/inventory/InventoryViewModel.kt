4c7d2a3603f8772b317af0559bd015c9bca70e71
1Sebi <sebastien.pnntr@eduge.ch>
2023-03-20 14:02:15 +0100

    Partie 2 etpae 05-07

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -15,6 +15,16 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
         return (item.quantityInStock > 0)
     }
 
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val updatedItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
+    }
+
     private fun updateItem(item: Item) {
         viewModelScope.launch {
             itemDao.update(item)
@@ -59,6 +69,19 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
 
     private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item{
         return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        return Item(
+            id = itemId,
             itemName = itemName,
             itemPrice = itemPrice.toDouble(),
             quantityInStock = itemCount.toInt()
